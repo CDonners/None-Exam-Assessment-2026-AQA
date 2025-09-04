5e55c51bde9a13a15787cdf1bdbbb631af4b2904
Python <python@python0002.co.uk>
2025-09-04 21:20:10 +0100

    Update countingCards.py
    Laying down the foundations pt2

--- CodeSnippets&Testing/countingCards.py
+++ CodeSnippets&Testing/countingCards.py
@@ -4,4 +4,13 @@ class countingCards():
         self.trueCount = 0
     
     def decisionMaking(self, hand: list):
-        handValue = hand[0].value
+        handValue = 0
+        for card in hand:
+            handValue += card.score
+        # Target is 21, 20, 19
+        if handValue in [21, 20, 19]:
+            return "Stand"
+        # Find cards needed:
+        valuesNeeded = [i-handValue for i in range(21, 18, -1)]
+        # Higher the count, the more high cards there are, JQK are high, 789 are mid, 456 are lowish, 23 are low A, is low and high
+        
