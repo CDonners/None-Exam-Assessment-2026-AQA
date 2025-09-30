22026efe1977baae58f07930e1fe2b540a1ad28d
CDonners <181085012+CDonners@users.noreply.github.com>
2025-09-30 12:35:33 +0100

    Cleaning Up

--- cardLogic/__init__.py
+++
@@ -1,1 +0,0 @@
-from .deckLogic import cards, playCards
--- game.py
+++
@@ -1,7 +0,0 @@
-from cardLogic import cards, playCards
-
-playCards = playCards(4)
-playCards.shuffle()
-
-for i in playCards.deck:
-    print(i.value)
