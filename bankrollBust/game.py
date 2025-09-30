b43e5829a5ba8a8df5458bf36a5727ec8edd203d
CDonners <181085012+CDonners@users.noreply.github.com>
2025-09-30 15:17:24 +0100

    Update game.py

--- bankrollBust/game.py
+++ bankrollBust/game.py
@@ -6,12 +6,21 @@ NPCPERSONALITIES = []
 
 class playGame():
     def __init__(self, noOfNPCs: int, startingBux: int):
+        self.user = None
         self.NPCs = []
         if noOfNPCs != 0: # Create NPCs
             for _ in range(noOfNPCs):
                 pass
-    def createPlayerObject(self, startingBux: int, personality = None, pr = None):
+            
+    def createPlayerObject(self, name: str, 
+                 bustBux: int, 
+                 personality: str, 
+                 prosperity: float,
+                 confidence: float, 
+                 judgement: float,
+                 experience: float):
         if personality is None: # Player Object is not an NPC so treat as such
-            pass
-        else:
-            pass
+            self.user = player(name, bustBux)
+        else: # Create an NPC
+            playerObject = NPC(name, bustBux, personality, prosperity, confidence, judgement, experience)
+            return playerObject
