c5dcae9e427883839b10fc220590468327f2eff4
CDonners <181085012+CDonners@users.noreply.github.com>
2025-12-08 13:09:58 +0000

    Buig Changes Coming

--- bankrollBust/main.py
+++ bankrollBust/main.py
@@ -1,6 +1,6 @@
 import pygame
 from deckLogic import deckHandling
-from pygameUtils.buttonUtils import button, discreteSlider
+from pygameUtils.buttonUtils import button, discreteSlider, inputBox
 
 deckInstance = deckHandling(4) # Deck handling instance ready
 
@@ -14,36 +14,39 @@ BGIMAGE = pygame.image.load("bankrollBust/images/table.png") # Load the backgrou
 bg = pygame.transform.scale(BGIMAGE, INITIALSIZE) # Set the size of the background image to the size of the screen
 
 # Button Setup
-newGameButton = button(screen, (700,300), "button")
-continueButton = button(screen, (700,400), "button")
-settingsButton = button(screen,(700,500), "button")
-quitButton = button(screen, (700,600), "button")
+newGameButton = button(screen, (700,300), "New Game")
+continueButton = button(screen, (700,400), "Continue")
+settingsButton = button(screen,(700,500), "Settings")
+quitButton = button(screen, (700,600), "Quit")
 
 def newGameSettings():
     # Mini Window Setup
     miniWindowImage = pygame.image.load("bankrollBust/images/MiniMenu.png")
-    miniWindowRect = miniWindowImage.get_rect(center=(initX/2, initY/2))
-    
+    miniWindowRect = miniWindowImage.get_rect(center=(initX/2, initY/2))    
     # Interaction Setup
-    noOfDecksSlider = discreteSlider(screen, (miniWindowRect.centerx, miniWindowRect.centery), [1,2,3,4,5])
-    difficultySlider = None
-    noOfPlayersSliders = None
-    startingBuxInput = None
-    #startButton = button(screen, (miniWindowRect.centerx, miniWindowRect.centery), "button")
-    #cancelButton = button(screen, (), "button")
+    noOfDecksSlider = discreteSlider(screen, "Number Of Decks", (miniWindowRect.centerx, miniWindowRect.centery-150), [1,2,4,6,8,10], scale=1.3)
+    difficultySlider = discreteSlider(screen, "Difficulty", (miniWindowRect.centerx, miniWindowRect.centery-100), ["Full-Assist", "Semi-Assist", "There-When-Needed","No-Help"], scale=1.3)
+    noOfNPCsSlider = discreteSlider(screen, "Number of NPCs", (miniWindowRect.centerx, miniWindowRect.centery-50), [0,1,2,3,4,5,6,7], scale=1.3)
+    startingBuxInput = inputBox(screen, (miniWindowRect.centerx, miniWindowRect.centery+25), "Starting Bux", None, scale=0.8)
+    startButton = button(screen, (miniWindowRect.centerx+135, miniWindowRect.centery+125), "Start Game", scale=0.7)
+    cancelButton = button(screen, (miniWindowRect.centerx-135, miniWindowRect.centery+125), "Cancel", scale=0.7)
     # Keeping Window Open
     started = False
     while not started:
         for event in pygame.event.get(): #Checking for events
             if event.type == pygame.QUIT: # If the user presses the X button, quit game
                 quit()
-        pygame.display.flip()
-        screen.blit(miniWindowImage, miniWindowRect)
-        if noOfDecksSlider.moveThumb():
-            pass
-            #return [1,2,3,4]
-        #if quitButton.updateImage():
-            #return None
+            screen.blit(miniWindowImage, miniWindowRect)
+            noOfDecks = noOfDecksSlider.getValue(event)
+            difficulty = difficultySlider.getValue(event)
+            noOfPlayers = noOfNPCsSlider.getValue(event)
+            startingBux = startingBuxInput.getInput(event)
+            if startButton.updateImage(event):
+                started = True
+                return [noOfDecks, difficulty, noOfPlayers, startingBux]
+            elif cancelButton.updateImage(event):
+                return None # Gotta figure out logic
+            pygame.display.flip()
 
 def newGame(noOfDecks, difficulty, startingBux, noOfPlayers):
     global gameRunning
@@ -57,23 +60,27 @@ def newGame(noOfDecks, difficulty, startingBux, noOfPlayers):
 # Main Game Loop
 gameRunning = True
 while gameRunning: # Main Menu Loop
+    screen.blit(bg, (0,0)) # Set the screen as my background
     for event in pygame.event.get(): #Checking for events
         if event.type == pygame.QUIT: # If the user presses the X button, quit game
             gameRunning = False
-    screen.blit(bg, (0,0)) # Set the screen as my background
-    # Draw the buttons, also detects whether pressed or 
-    if newGameButton.updateImage():
-        gameSettings = newGameSettings()
-        if newGameSettings is not None: # If it is none they cancelled new game
-            noOfDecks = gameSettings[0]
-            difficulty = gameSettings[1]
-            startingBux = gameSettings[2]
-            noOfPlayers = gameSettings[3]
-    continueButton.updateImage()
-    settingsButton.updateImage()
-    if quitButton.updateImage():
-        gameRunning = False
+        if newGameButton.updateImage(event):
+            gameSettings = newGameSettings()
+            if gameSettings is not None: # If it is none they cancelled new game
+                noOfDecks = gameSettings[0]
+                difficulty = gameSettings[1]
+                noOfPlayers = gameSettings[2]
+                startingBux = gameSettings[3]
+                newGame(noOfDecks, difficulty, noOfDecks, startingBux)
+        elif continueButton.updateImage(event):
+            pass
+        elif settingsButton.updateImage(event):
+            pass
+        elif quitButton.updateImage(event):
+            gameRunning = False
+        pygame.display.flip() # Update the screen
     
-    pygame.display.flip() # Update the screen
+    
     
 pygame.quit # Exit out of Pygame
+
--- bankrollBust/pygameUtils/buttonUtils.py
+++ bankrollBust/pygameUtils/buttonUtils.py
@@ -5,49 +5,67 @@ CWD = os.getcwd() # Get the current working directory
 BIFD = CWD + "\\bankrollBust\\images\\buttons\\" # Button Images Folder Directory
 
 class button:
-    def __init__(self, surface, centre, buttonName):#
-        self.bid = BIFD + buttonName
-        self.centre = centre
-        self.interactableImage = pygame.image.load(self.bid+".png")
-        self.interactableObj = self.interactableImage.get_rect(center = centre)
-        self.surface = surface
-    
-    def draw(self):
-        self.surface.blit(self.interactableImage, self.interactableObj)
+    def __init__(self, surface: pygame.Surface, centre: tuple, name: str, scale = 1.0):
+        # Variables
+        self.bid = BIFD + "button" # BID: Button Image Directory
+        self.centre = centre # Centre of the button
+        self.surface = surface # Defining the surface
+        self.scale = scale # Defining the scale
+        # Creating the interactable button
+        self.interactableImage = pygame.transform.scale_by(pygame.image.load(self.bid+".png"), scale) # Loading the image and adjusting it to the specified scale
+        self.interactableObj = self.interactableImage.get_rect(center = centre) # Turn the image into a rect with the specified centre
+        # Create text object
+        self.font = pygame.font.SysFont("", 36)
+        self.text_surface = self.font.render(f"{name}", True, (255, 255, 255))
+        self.text_rect = self.text_surface.get_rect(center=centre)
         
-    def checkHover(self):
+    
+    def draw(self): # Blits the rect onto the surface
+        self.surface.blit(self.interactableImage, self.interactableObj)
+        self.surface.blit(self.text_surface, self.text_rect)
+        
+    def checkHover(self): # Checks if the mouse is hovered over the rect
         mousePos = pygame.mouse.get_pos()
         return self.interactableObj.collidepoint(mousePos)
 
-    def checkPressed(self):
-        if pygame.mouse.get_pressed()[0]:
-            return True
+    def checkPressed(self, event): # Checks if the mouse is pressed while it hovers over the button
+        if event.type == pygame.MOUSEBUTTONDOWN: # Checking if the button press is a mouse press
+            if event.button == 1: # Checks if the button pressed is the left mouse button
+                return True
         
-    def updateImage(self):
-        pressed = False
+    def updateImage(self, event): # Updates the image depending on how the button is being interacted with
+        pressed = False 
         if self.checkHover(): # Check if the mouse is over the button
-            if self.checkPressed(): # Button is Pressed
-                self.interactableImage = pygame.image.load(self.bid+"_pressed"+".png")
+            if self.checkPressed(event): # Button is Pressed
+                self.interactableImage = pygame.transform.scale_by(pygame.image.load(self.bid+"_pressed"+".png"),self.scale)
                 pressed = True
             else: # Buttons isn't pressed
-                self.interactableImage = pygame.image.load(self.bid+"_hovered"+".png")
+                self.interactableImage = pygame.transform.scale_by(pygame.image.load(self.bid+"_hovered"+".png"), self.scale)
         else: # Button isn't being interacted with
-            self.interactableImage = pygame.image.load(self.bid+".png")
+            self.interactableImage = pygame.transform.scale_by(pygame.image.load(self.bid+".png"), self.scale)
         self.draw() # Draw the updated image
         return pressed
         
 class discreteSlider(button):
-    def __init__(self, surface: pygame.Surface, centre: tuple, values: list):
+    def __init__(self, surface: pygame.Surface, name: str, centre: tuple, values: list, scale = 1.0):
+        # Variables
         self.surface = surface
+        self.name = name
         self.centre = centre
         self.values = values
-        self.guideImage = pygame.image.load(BIFD + "sliderGuide.png")
+        self.scale = scale
+        self.value = values[0]
+        # Creating Slider Guide
+        self.guideImage = pygame.transform.scale_by(pygame.image.load(BIFD + "sliderGuide.png"), self.scale)
         self.guideRect = self.guideImage.get_rect(center = centre)
+        # Variables for slider thumb
         self.thumbLBound = self.guideRect.left
         self.thumbUBound = self.guideRect.right
-        self.interactableImage = pygame.image.load(BIFD + "thumb.png")
+        # Creating the rect objects for the slider
+        self.interactableImage = pygame.transform.scale_by(pygame.image.load(BIFD + "thumb.png"), self.scale)
         self.interactableObj = self.interactableImage.get_rect(center = (self.thumbLBound, self.guideRect.centery))
-        self.pressed = False
+        # Variables for Logic
+        self.mouseHeld = False
         self.valuePoints = self.getValuePoints()
         
     def getValuePoints(self): # Figures out where a point should be on the slider
@@ -80,26 +98,107 @@ class discreteSlider(button):
         else:
             return upperBound
                     
-
-    def moveThumb(self):
-        mousePos = pygame.mouse.get_pos()
-        offsetX = 0
-        if self.pressed == False:
-            if self.checkHover():
-                self.interactableImage = pygame.image.load(BIFD + "thumbHovered.png")
-                if self.checkPressed():
-                    offsetX = mousePos[0] - self.interactableObj.centerx
-                    self.pressed = True
-            else:
-                self.interactableImage = pygame.image.load(BIFD + "thumb.png")
-        if self.pressed == True:
-            if mousePos[0] > self.thumbLBound and mousePos[0] < self.thumbUBound:
-                self.interactableObj.centerx = mousePos[0]-offsetX
-            if not self.checkPressed():
-                self.pressed = False
-                snapPos = self.snapThumb()
-                self.interactableObj.centerx = snapPos
+    def draw(self):
+        # Draw thumb and guide
         self.surface.blit(self.guideImage, self.guideRect)
         self.surface.blit(self.interactableImage, self.interactableObj)
+        # Create the text object
+        font = pygame.font.SysFont("", 22)
+        text_surface = font.render(f"{self.name}: {self.value}", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(self.centre[0], self.centre[1]-15))
+        # Draw Text
+        self.surface.blit(text_surface, text_rect)
+        # For visibility of Nodes during testing
         # for i in list(self.valuePoints.keys()):
         #     pygame.draw.circle(self.surface, (0,255,0), (i,self.guideRect.centery), 5)
+
+    def moveThumb(self, event):
+        mousePos = pygame.mouse.get_pos()
+        offsetX = 0
+        if self.mouseHeld == False:
+            if self.checkHover():
+                self.interactableImage = pygame.transform.scale_by(pygame.image.load(BIFD + "thumbHovered.png"), self.scale)
+                self.checkPressed(event)
+                if self.mouseHeld:
+                    offsetX = mousePos[0] - self.interactableObj.centerx
+            else:
+                self.interactableImage = pygame.transform.scale_by(pygame.image.load(BIFD + "thumb.png"), self.scale)
+        if self.mouseHeld == True:
+            self.checkPressed(event)
+            if mousePos[0] > self.thumbLBound and mousePos[0] < self.thumbUBound:
+                self.interactableObj.centerx = mousePos[0]-offsetX
+            if not self.mouseHeld:
+                snapPos = self.snapThumb()
+                self.interactableObj.centerx = snapPos
+        
+    def getValue(self, event):
+        self.moveThumb(event)
+        self.draw()
+        closestValue = self.snapThumb()
+        self.value = self.valuePoints[closestValue]
+        return self.value
+    
+    def checkPressed(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                self.mouseHeld = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                self.mouseHeld = False
+                
+class inputBox(button):
+    def __init__(self, surface: pygame.Surface, centre: tuple, name: str, inputType, scale=1.0):
+        # Variables
+        self.surface = surface
+        self.centre = centre
+        self.name = name
+        self.inputType = inputType
+        self.scale = scale
+        self.selected = False
+        self.value = "1000" # Setting to a default value
+        # Creating the input box
+        self.interactableImage = pygame.transform.scale_by(pygame.image.load(BIFD + "button.png"), self.scale)
+        self.interactableObj = self.interactableImage.get_rect(center = centre)
+        self.title_font = pygame.font.SysFont("", 22)
+        self.title_text_surface = self.title_font.render(f"{self.name}", True, (255, 255, 255))
+        self.title_text_rect = self.title_text_surface.get_rect(center=(self.centre[0], self.centre[1]-45))
+        
+    def pressed(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                return True
+        return False
+    
+    def draw(self):
+        # Draw thumb and guide
+        self.surface.blit(self.interactableImage, self.interactableObj)
+        # Create the text object for value
+        self.input_font = pygame.font.SysFont("", 22)
+        self.input_text_surface = self.title_font.render(f"{self.value}", True, (255, 255, 255))
+        self.input_text_rect = self.title_text_surface.get_rect(center=self.centre)
+        # Draw Text
+        self.surface.blit(self.title_text_surface, self.title_text_rect)
+        self.surface.blit(self.input_text_surface, self.input_text_rect)
+        ## For visibility of Nodes during testing
+        # for i in list(self.valuePoints.keys()):
+        #     pygame.draw.circle(self.surface, (0,255,0), (i,self.guideRect.centery), 5)
+    
+    def checkSelected(self, event):
+        if self.pressed(event):
+            if self.checkHover():
+                self.selected = True
+                self.value = ""
+            else:
+                self.selected = False
+                if self.selected == "":
+                    self.selected = "1000"
+    
+    def getInput(self, event):
+        self.draw()
+        self.checkSelected(event)
+        if self.selected:
+            if event.type == pygame.KEYDOWN:
+                key_pressed = pygame.key.name(event.key)
+                if key_pressed.isnumeric():
+                    self.value = self.value + key_pressed
+        return self.value            
