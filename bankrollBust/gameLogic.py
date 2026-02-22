97daf38de5ba4c61f2a06f126078124c2cb1cb88
Python <python@python0002.co.uk>
2026-02-22 21:02:24 +0000

    Seating Positions

--- bankrollBust/gameLogic.py
+++ bankrollBust/gameLogic.py
@@ -67,7 +67,7 @@ class playGame():
         self.user = player(startingBux) # Creating the player object
         self.NPCs = [] # Preparing to make the NPCs
         self.roundStarted = False
-        self.playerPositions = {}
+        self.playerSeats = {}
         self.gameSetup = setupGame(startingBux)
         deckInstance = deckHandling(noOfDecks) # Deck handling instance ready
         if noOfNPCs != 0: # Create NPCs
@@ -75,7 +75,7 @@ class playGame():
                 self.NPCs.append(self.gameSetup.createNPC())
         self.players = self.NPCs[:] # A list for all the players, cloning the NPCs list
         self.players.insert((len(self.NPCs))//2, self.user) # Insert the User object into the middle of the list with a left bias
-        self.getPlayerPositions()
+        self.getPlayerSeats()
         
     def initialDeal(self):
         for _ in range(2): # Deal 2 cards to players from left to right
@@ -83,16 +83,23 @@ class playGame():
                 player.dealCard(deckInstance)
         self.roundStarted = True
 
-    def getPlayerPositions(self):
-        # Place the players in their positions
-        allPositions = [(), (), (), (), (), (), ()] # Positions are preset
-        middlePlayerIndex = len(self.players) // 2 # Get the middle position of players
-        startingPos = 6 - middlePlayerIndex
-        for index in range(startingPos, len(self.players)+startingPos):
-            playerPosIndex = index - startingPos # Position of the player in the list
-            playerToAssign = self.players[playerPosIndex]
-            self.playerPositions[playerToAssign] = allPositions[index]
+    def getPlayerSeats(self):
+        # Assigns players their seating positions
+        seatingPositions = [(), (), (), (), (), (), ()]
+        playerPos = 0 # Position of the player in the list
+        # Get the position of the user in the players list
+        for i in self.players:
+            if i.name == "Player":
+                playerPos = self.players.index(i)
+        # Align the 2 lists so the middles match
+        startingPos = 3 - playerPos # 3 is the middle of the seatingPositions list, player pos should be the middle of the players list
+        for i in range(startingPos, len(self.players) + startingPos):
+            # self.players[i - startingPos] "i - startingPos" Is the index of the players in the player list
+            # seatingPositions[i] is the aligned seating position
+            self.playerSeats[self.players[i - startingPos]] = seatingPositions[i]
+        
         
     def updateImage(self):
         for i in self.players:
             pass
+
