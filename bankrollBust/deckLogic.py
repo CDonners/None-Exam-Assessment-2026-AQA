8ba30743d889f951d6370b006b570ead7f0f09ec
Python <python@python0002.co.uk>
2026-02-24 22:11:47 +0000

    Fixing typo

--- bankrollBust/deckLogic.py
+++ bankrollBust/deckLogic.py
@@ -10,6 +10,8 @@ class cards():
         self.cardWeight = cardWeight
         self.visible = False
         self.discarded = False
+        self.cardRect = None
+        self.cardText = None
         
     def setVisible(self):
         self.visible = True
@@ -29,7 +31,7 @@ class deckHandling():
     def generateDeck(self, noOfDecks: int):
         # Card Values and Suits
         suits = ["h","d","s","c"]
-        values = [["A", -1],["2", 1],["3", 1],["4", 2],["5", 2],["6", 2],["7", 1],["8", 0],["9", 0],["10", -2],["J", -2],["Q", -3],["K", -2]] 
+        values = [["A", -1],["2", 1],["3", 1],["4", 2],["5", 2],["6", 2],["7", 1],["8", 0],["9", 0],["10", -2],["J", -2],["Q", -2],["K", -2]] 
         deck = [] # Empty list to append generated cards to
         # Create every card in the standard 52 playing card deck
         for suit in suits:
